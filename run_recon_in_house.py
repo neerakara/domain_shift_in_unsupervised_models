573601e91034fa1f83404552123bf85322a56d30
Neerav Karani <nkarani@ethz.ch>
2020-08-12 18:23:48 +0200

    added function to update normalizer

--- run_recon_in_house.py
+++ run_recon_in_house.py
@@ -5,6 +5,7 @@ from US_pattern import US_pattern
 import argparse
 import h5py
 import utils
+import os
 
 parser = argparse.ArgumentParser(prog = 'PROG')
 parser.add_argument('--sli', type = int, default = 3) 
@@ -13,7 +14,11 @@ parser.add_argument('--usfact', type = float, default = 2)
 parser.add_argument('--contrun', type = int, default = 0) 
 args=parser.parse_args()
 
-basefolder = '/usr/bmicnas01/data-biwi-01/nkarani/projects/domain_shift_unsupervised/data/'
+basefolder = '/usr/bmicnas01/data-biwi-01/nkarani/projects/domain_shift_unsupervised/'
+results_folder = basefolder + 'code/v2.0/results/in_house/' + args.base
+if not os.path.exists(results_folder):
+    os.makedirs(results_folder)    
+    
 ndims = 28
 lat_dim = 60
 mode = 'MRIunproc' #'Melanie_BFC'
@@ -25,7 +30,7 @@ if np.floor(usfact) == usfact: # if it is already an integer
 # =======================================================================================
 # RECON
 # =======================================================================================
-dirbase = basefolder + 'the_h5_files/' + args.base
+dirbase = basefolder + 'data/the_h5_files/' + args.base
 sli = args.sli
 ddr = np.array((h5py.File(dirbase+'ddr_sl'+str(sli)+'.h5', 'r')['DS1']))
 ddi = np.array((h5py.File(dirbase+'ddi_sl'+str(sli)+'.h5', 'r')['DS1']))
@@ -62,11 +67,11 @@ R = usfact
 # undersampling pattern. either load or generate.
 # ======================================
 try:
-     uspat = np.load(basefolder + 'uspats/uspat_realim_us' + str(R) + '_base_' + dirbase[-19:-1].replace("/","_") + '_sli' + str(sli) + '.npy')
+     uspat = np.load(basefolder + 'data/uspats/uspat_realim_us' + str(R) + '_base_' + dirbase[-19:-1].replace("/","_") + '_sli' + str(sli) + '.npy')
      print("Read from existing u.s. pattern file")
 except:
      uspat = USp.generate_opt_US_pattern_1D(dd.shape[0:2], R=R, max_iter=100, no_of_training_profs=15)
-     np.save(basefolder + 'uspats/uspat_realim_us' + str(R) + '_base_' + dirbase[-19:-1].replace("/","_") + '_sli' + str(sli), uspat)
+     np.save(basefolder + 'data/uspats/uspat_realim_us' + str(R) + '_base_' + dirbase[-19:-1].replace("/","_") + '_sli' + str(sli), uspat)
      print("Generated a new u.s. pattern file")
           
 usksp = dd*np.tile(uspat[:,:,np.newaxis], [1, 1, dd.shape[2]])
@@ -75,27 +80,36 @@ num_iter = 102
 regtype = 'reg2'
 reg = 0 # no phase regulization!
 dcprojiter = 10
-onlydciter = 10 # do firist iterations only SENSE reconstruction
+onlydciter = 10 # do first iterations only SENSE reconstruction
 chunks40 = True
 mode = 'MRIunproc'
+n1 = 0 # number of updates of the normalization module
+n2 = 10 # number of updates of the image
+recon_suffix = 'us' + str(R) + '_sli' + str(sli) + '_regtype_' + regtype + '_reglambda_' + str(reg) + '_n1_' + str(n1) +  '_n2_' + str(n2) 
   
 # =============================
 # do the reconstruction
 # =============================          
 rec_vae = vaerecon.vaerecon(usksp,
                             sensmaps = sensmaps,
-                            dcprojiter = 10,
-                            onlydciter = 10,
+                            dcprojiter = dcprojiter,
+                            onlydciter = onlydciter,
                             lat_dim = lat_dim,
                             patchsize = ndims,
                             contRec = '',
                             parfact = 25,
-                            num_iter = 302,
+                            num_iter = num_iter,
                             regiter = 10,
                             reglmb = reg,
                             regtype = regtype,
                             half = True,
                             mode = mode,
-                            chunks40 = chunks40)
+                            chunks40 = chunks40,
+                            n1 = n1,
+                            n2 = n2)
 
-pickle.dump(rec_vae[0], open(basefolder + 'MAPestimation/rec_meas_us' + str(R) + '_sli' + str(sli) + '_regtype_' + regtype + '_dcprojiter_' + str(dcprojiter) ,'wb'))
+# =============================   
+# write results to disk
+# =============================   
+pickle.dump(rec_vae[0], open(results_folder + recon_suffix, 'wb'))
+
